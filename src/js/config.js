925786c61a24ec79d3a9dd575505559cb82ccff4
MarkoZastavnikovic <marko.zastavnikovic@hotmail.com>
2022-05-26 17:55:52 +0200

    Adding background img for recipe view

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 12;
+export const RES_PER_PAGE = 10;
 export const KEY = '7d69e3af-86ea-4237-a7c6-3fecdd756096';
 export const MODAL_CLOSE_SEC = 2.5;
